741979374db1a5d75d34cc5f160da9b2d999a79f
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-03-09 07:16:09 +0900

    useCallbackとuseMemo

--- re-rendering-udemy/App.jsx
+++ re-rendering-udemy/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea"; // auto importされる
 import "./styles.css";
 
@@ -26,13 +26,27 @@ export const App = () => {
   // 現在とは逆の値に切り替える trueならfalseに更新する動き
   const onClickOpen = () => setOpen(!open);
 
+  /** アロー関数：毎回新しい関数を生成しているとみなされる
+   * propsが変更しているとみなされChildAreaも再レンダリングされる
+   * ChildArea側でmemo化していても、onClickCLoseは違うものと判断され毎回再レンダリングされる
+    → useCallbackを使う！！！！
+    []空にしておくと、最初に生成したものを使い回すようになる
+   */
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  /** 変数自体のメモ化 useMemo 頻繁には使わない
+   * 最初に読み込まれた時だけ計算が行われる。tempは４のまま使い回される
+   */
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 };
