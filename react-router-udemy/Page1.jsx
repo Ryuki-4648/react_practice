011a6a292da766f0b1d9242b3545c982df589c7b
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-03-17 06:19:26 +0900

    stateを渡すページ遷移

--- react-router-udemy/Page1.jsx
+++ react-router-udemy/Page1.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
+  // この配列を遷移先のDetailAに渡したい
+  // stateの中に変数を持たせると、情報を持ったままルーティングさせることができる
+  // 一覧から詳細に遷移する時に無駄なAPIアクセスをするのを防ぐなどに使える
   return (
     <div>
       <h1>Page1ページです</h1>
-      <Link to="/page1/detaila">Detail A</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>Detail A</Link>
       <br />
-      <Link to="/page1/detailb">Detail B</Link>
+      <Link to="/page1/detailB">Detail B</Link>
     </div>
   );
 };
--- react-router-udemy/Page1DetailA.jsx
+++ react-router-udemy/Page1DetailA.jsx
@@ -1,4 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const location = useLocation();
+  console.log(location);
+  // pathname: "/page1/detailA", state: Array(100),...
+
+  const { state } = useLocation();
+  console.log(state);
+  // (100) [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, …]
+
   return (
     <div>
       <h1>Page1DetailAページです</h1>
