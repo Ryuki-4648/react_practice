cbbdb60787b223bfc51639c032442bb6f6a9c227
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-03-17 06:29:42 +0900

    Linkを使わずにページ遷移。js側での遷移

--- react-router-udemy/Page1.jsx
+++ react-router-udemy/Page1.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
@@ -6,12 +6,21 @@ export const Page1 = () => {
   // この配列を遷移先のDetailAに渡したい
   // stateの中に変数を持たせると、情報を持ったままルーティングさせることができる
   // 一覧から詳細に遷移する時に無駄なAPIアクセスをするのを防ぐなどに使える
+
+  const history = useHistory();
+  // useHistoryというフックを変数に展開
+
+  const onClickDetailA = () => history.push("/page1/detailA");
+  // js側から遷移することが可能 pushメソッドの引数に遷移先のルートを記述
+
   return (
     <div>
       <h1>Page1ページです</h1>
       <Link to={{ pathname: "/page1/detailA", state: arr }}>Detail A</Link>
       <br />
       <Link to="/page1/detailB">Detail B</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
--- react-router-udemy/Page1DetailA.jsx
+++ react-router-udemy/Page1DetailA.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const location = useLocation();
@@ -9,9 +9,15 @@ export const Page1DetailA = () => {
   console.log(state);
   // (100) [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, …]
 
+  const history = useHistory();
+
+  const onClickBack = () => history.goBack();
+  // goBackを呼び出す。ブラウザのバックボタンを同じ挙動
+
   return (
     <div>
       <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
