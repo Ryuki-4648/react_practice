02af7457b6e880061b0bd562b723e85b9d176116
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-03-18 06:52:45 +0900

    atomの作成 共通スタイルをまとめる

---
+++ react-atom/components/atoms/button/BaseButton.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  color: #fff;
+  padding: 6px 24px;
+  border: none;
+  outline: none;
+  transition: all 0.3s;
+  cursor: pointer;
+  border-radius: 9999px;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
--- react-atom/components/atoms/button/PrimaryButton.jsx
+++ react-atom/components/atoms/button/PrimaryButton.jsx
@@ -1,24 +1,11 @@
-/**
- * このコンポーネントは何の役割があるのかを理解しておく
- */
-
 import styled from "styled-components";
+import { BaseButton } from "./BaseButon";
 
-export const PrimaryButton = (props) => {
+export const SecondaryButton = (props) => {
   const { children } = props;
   return <SButton>{children}</SButton>;
 };
 
-const SButton = styled.button`
-  background-color: #40415e;
-  color: #fff;
-  padding: 6px 24px;
-  border: none;
-  outline: none;
-  transition: all 0.3s;
-  cursor: pointer;
-  border-radius: 9999px;
-  &:hover {
-    opacity: 0.8;
-  }
+const SButton = styled(BaseButton)`
+  background-color: #11999e;
 `;
