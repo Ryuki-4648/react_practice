26cbccd0a9fab3b8d669afedbb0fe1750fc17768
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-03-24 06:39:03 +0900

    pageの作成

--- react-atom/components/pages/Users.jsx
+++ react-atom/components/pages/Users.jsx
@@ -1,27 +1,32 @@
 import styled from "styled-components";
 import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
 
 // 何人分も必要なので、配列を作成していく
-const users = [...Array(10).keys()].map(() => {
-  return {};
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `$名前-${val}`,
+    image: "https://source.unsplash.com/2l0CWTpcChI",
+    email: "12345@aaa.com",
+    phone: "1211-444-3333",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
 });
-
-const user = {
-  name: "yuki",
-  image: "https://source.unsplash.com/2l0CWTpcChI",
-  email: "12345@aaa.com",
-  phone: "1211-444-3333",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
@@ -31,4 +36,12 @@ const SContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
