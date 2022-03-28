db1cd3b689b52723d7bd21b4d55a9fe7dfbe6ffb
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-03-29 06:43:28 +0900

    propsの型を指定しない場合

--- react-typescript/src/Todo.tsx
+++ react-typescript/src/Todo.tsx
@@ -1,4 +1,9 @@
 export const Todo = (props) => {
-  const { title, userid } = props;
-  return <p>{`${title}(ユーザー:${userid})`}</p>;
+  const { title, userid, completed } = props;
+
+  const completeMark = completed ? "[完]" : "[未]";
+
+  return <p>{`${completeMark} ${title}(ユーザー:${userid})`}</p>;
 };
+
+// propsの型指定していない場合。親コンポーネントからcompletedを渡されていないけどコード上エラーになっていない
