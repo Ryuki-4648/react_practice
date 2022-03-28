82b81859adb89fd8382d94e2e6bbe2145d1462b3
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-03-29 06:33:26 +0900

    取得データの型を指定する

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,15 +1,40 @@
 import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+/**元の形
+ * "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+ */
 
 export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
   const onCLickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res); // {data: Array(200),....
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        //console.log(res); // {data: Array(200),....
+
+        //res.data.map((todo) => todo.userId)
+        setTodos(res.data); // 文字列を渡すとエラーになる
+      });
   };
   return (
     <div className="App">
       <button onClick={onCLickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userId} />
+      ))}
     </div>
   );
 }
