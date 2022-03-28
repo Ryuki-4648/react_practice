8efe1e5d452e34e0a46dcc46ceaf2348e09a5359
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-03-28 22:37:42 +0900

    React×Typescriptの事前準備

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import "./styles.css";
+
+export default function App() {
+  const onCLickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res); // {data: Array(200),....
+    });
+  };
+  return (
+    <div className="App">
+      <button onClick={onCLickFetchData}>データ取得</button>
+    </div>
+  );
+}
