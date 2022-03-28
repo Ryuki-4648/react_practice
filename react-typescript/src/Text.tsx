36ada6aa8e3c1e75d4f4184381a4ceb8eee63b07
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-03-29 07:21:26 +0900

    propsの型指定　FC

--- react-typescript/src/Text.tsx
+++ react-typescript/src/Text.tsx
@@ -1,9 +1,12 @@
+import { FC } from "react";
+// 関数コンポーネントの型 childrenを受け取れる
+
 type Props = {
   color: string;
   fontSize: string;
 };
 
-export const Text = (props: Props) => {
+export const Text: FC<Props> = (props) => {
   const { color, fontSize } = props;
   return <p style={{ color, fontSize }}>テキストです</p>;
 };
