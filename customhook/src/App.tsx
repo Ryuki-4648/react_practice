dd589a61f73c8f17f3e9e8c712c1201c887fda18
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-03-30 06:59:36 +0900

    カスタムフックを使わない場合　途中

--- customhook/src/App.tsx
+++ customhook/src/App.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { useState } from "react";
 import { UserCard } from "./components/UserCard";
 import "./styles.css";
 import { User } from "./types/api/user";
+import { UserProfile } from "./types/userProfile";
 
 const user = {
   id: 1,
@@ -11,13 +13,27 @@ const user = {
 };
 
 export default function App() {
+  const [UserProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
   const onClickFetchUser = () => {
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users");
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      <UserCard user={user} />
+      {UserProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
