ba75a2e7a3a370d84ac0b2868f9b4c44bd03acf2
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-04 06:37:25 +0900

    ChakraUIの準備、Reactエラー解消

--- chakra/src/App.tsx
+++ chakra/src/App.tsx
@@ -1,7 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 import "./styles.css";
 
 export default function App() {
-  return;
-  <ChakraProvider></ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <Button colorScheme="teal">ボタン</Button>
+    </ChakraProvider>
+  );
 }
