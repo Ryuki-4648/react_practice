b372ee9022fbad6ac8842e05b5ac5c2cba40b42b
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-04 07:49:43 +0900

    404ページの作成

--- chakra/src/router/HomeRoutes.tsx
+++ chakra/src/router/HomeRoutes.tsx
@@ -1,6 +1,7 @@
 import { Home } from "../components/pages/Home";
 import { UserManagement } from "../components/pages/UserManagement";
 import { Setting } from "../components/pages/Setting";
+import { Page404 } from "../components/pages/Page404";
 
 export const homeRoutes = [
   {
@@ -17,5 +18,12 @@ export const homeRoutes = [
     path: "/setting",
     exact: false,
     children: <Setting />
+  },
+  {
+    path: "*",
+    exact: false,
+    children: <Page404 />
   }
 ];
+
+// ↑home/配下で存在しないurlを入力された場合も４０４ページを表示させる
