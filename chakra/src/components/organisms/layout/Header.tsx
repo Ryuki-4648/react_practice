a864587fec37d5fe5e7a451f78335d93fd933b75
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-05 06:26:20 +0900

    MenuDrawerコンポーネント化

--- chakra/src/components/organisms/layout/Header.tsx
+++ chakra/src/components/organisms/layout/Header.tsx
@@ -53,17 +53,6 @@ export const Header: VFC = memo(() => {
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerBody p={0} bg="gray.100">
-              <Button w="100%">TOP</Button>
-              <Button w="100%">ユーザー一覧</Button>
-              <Button w="100%">設定</Button>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
     </>
   );
 });
