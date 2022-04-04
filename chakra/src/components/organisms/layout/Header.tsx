bd92917eff178ce1cbae17762ce0f7be14411d1d
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-04 21:37:21 +0900

    Headerの作成　途中　chakra ui,base

--- chakra/src/components/organisms/layout/Header.tsx
+++ chakra/src/components/organisms/layout/Header.tsx
@@ -1,5 +1,40 @@
+import { Box, Flex, Heading, Link } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+/**
+ * Flex:flexboxみたいなのができる
+ * as nav:ナビタグとしていく　　asをつけることで、どのタグとしてレンダリングするか指定できる
+ *
+ * 文字のサイズmd md以上にあったら文字を大きくする
+ *
+ * flexGrow:伸び率
+ */
 export const Header: VFC = memo(() => {
-  return <div style={{ height: "50px", backgroundColor: "teal" }}></div>;
+  return (
+    <Flex
+      as="nav"
+      bg="teal.500"
+      color="gray.50"
+      align="center"
+      justify="space-between"
+      padding={{ base: 3, md: 5 }}
+    >
+      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+          ユーザー管理アプリ
+        </Heading>
+      </Flex>
+      <Flex
+        align="center"
+        fontSize="sm"
+        flexGrow={2}
+        display={{ base: "none", md: "flex" }}
+      >
+        <Box pr={4}>
+          <Link>ユーザー一覧</Link>
+        </Box>
+        <Link>設定</Link>
+      </Flex>
+    </Flex>
+  );
 });
