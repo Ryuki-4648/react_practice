0edaaf666756010f94502fcb337b4c2144a8a188
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-04 22:16:33 +0900

    ハンバーガーメニューをコンポーネント化

--- chakra/src/components/atoms/button/MenuIconButton.tsx
+++ chakra/src/components/atoms/button/MenuIconButton.tsx
@@ -2,11 +2,20 @@ import { HamburgerIcon } from "@chakra-ui/icons";
 import { IconButton } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
-export const MenuIconButton: VFC = memo(() => {
+/**
+ * 引数のないただの関数　返却値もない
+ */
+type Props = {
+  onOpen: () => void;
+};
+
+export const MenuIconButton: VFC<Props> = memo((props) => {
+  const { onOpen } = props;
+
   return (
     <IconButton
       aria-label="メニューボタン"
-      icon={<HamburgerIcon/>}
+      icon={<HamburgerIcon />}
       size="sm"
       variant="unstyled"
       display={{ base: "block", md: "none" }}
