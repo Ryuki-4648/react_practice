5bd22d8f360a4222a2f5a563e12c911f0bee7783
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-07 06:37:43 +0900

    showMesssage propsとして受け取るものの設定

--- chakra/src/components/hooks/useMessage.ts
+++ chakra/src/components/hooks/useMessage.ts
@@ -1,16 +1,29 @@
 import { useToast } from "@chakra-ui/react";
 import { useCallback } from "react";
 
+type Props = {
+  title: string;
+  status: "info" | "warning" | "success" | "error"; // この4つの文字列しか受け取れない
+};
+
 export const useMessage = () => {
   // chakra ui toast
   const toast = useToast();
 
-  const showMessage = useCallback(() => {
-    toast({
-      position: "top",
-      duration: 2000, // message２秒間残る
-      isClosable: true // 閉じれるかどうか
-    });
-  }, []);
+  // 何をpropsとして受け取るか
+  const showMessage = useCallback(
+    (props: Props) => {
+      const { title, status } = props;
+
+      toast({
+        title, // 同じ名称なのでコロン以降省略できる
+        status,
+        position: "top",
+        duration: 2000, // message２秒間残る
+        isClosable: true // 閉じれるかどうか
+      });
+    },
+    [toast]
+  );
   return { showMessage };
 };
