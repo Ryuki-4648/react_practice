fab43134f2a9d2d35fec88acdf2226d8e2e7bc0b
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-07 07:24:31 +0900

    ユーザー一覧　スタイルあてる

--- chakra/src/components/pages/UserManagement.tsx
+++ chakra/src/components/pages/UserManagement.tsx
@@ -1,16 +1,36 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <Wrap spacing="30px">
-      {[...Array(10)].map(() => (
-        <WrapItem>
-          <div
-            style={{ width: "100px", height: "100px", backgroundColor: "teal" }}
-          ></div>
-        </WrapItem>
-      ))}
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          w="260"
+          h="260"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+          p={4}
+          _hover={{ cursor: "pointer", opacity: ".8" }}
+        >
+          <Stack textAlign="center">
+            <Image
+              borderRadius="full"
+              boxSize="160px"
+              src="https://source.unsplash.com/random"
+              alt="profile image"
+              m="auto"
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              your name
+            </Text>
+            <Text fontSize="sm" color="gray">
+              full name
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
     </Wrap>
   );
 });
