89cb0db089662437b17177a37a2c978ca9dcd2e8
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-07 06:33:18 +0900

    toastのオプション

--- chakra/src/components/hooks/useMessage.ts
+++ chakra/src/components/hooks/useMessage.ts
@@ -5,6 +5,12 @@ export const useMessage = () => {
   // chakra ui toast
   const toast = useToast();
 
-  const showMessage = useCallback(() => {}, []);
+  const showMessage = useCallback(() => {
+    toast({
+      position: "top",
+      duration: 2000, // message２秒間残る
+      isClosable: true // 閉じれるかどうか
+    });
+  }, []);
   return { showMessage };
 };
