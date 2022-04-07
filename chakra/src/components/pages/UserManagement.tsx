3e645b1c52b75d2cb1694e4bfde59cac1d85d5ce
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-08 07:28:33 +0900

    モーダルウィンドウの実装

--- chakra/src/components/pages/UserManagement.tsx
+++ chakra/src/components/pages/UserManagement.tsx
@@ -2,15 +2,22 @@
 
 import {
   Center,
+  FormControl,
+  FormLabel,
+  Input,
   Modal,
+  ModalBody,
+  ModalCloseButton,
   ModalContent,
+  ModalHeader,
   ModalOverlay,
   Spinner,
+  Stack,
   useDisclosure,
   Wrap,
   WrapItem
 } from "@chakra-ui/react";
-import { memo, useEffect, VFC } from "react";
+import { memo, useCallback, useEffect, VFC } from "react";
 
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
@@ -25,7 +32,7 @@ export const UserManagement: VFC = memo(() => {
   */
   useEffect(() => getUsers(), []);
 
-  const onClickUser = () => onOpen();
+  const onClickUser = useCallback(() => onOpen(), []);
 
   return (
     <>
@@ -47,10 +54,36 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        autoFocus={false}
+        motionPreset="slideInBottom"
+      >
         <ModalOverlay />
-        <ModalContent>
-          <p>test</p>
+        <ModalContent pb={6}>
+          <ModalHeader>ユーザー詳細</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mx={4}>
+            <Stack spacing={4}>
+              <FormControl>
+                <FormLabel>名前</FormLabel>
+                <Input value="name" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>フルネーム</FormLabel>
+                <Input value="full name" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>メール</FormLabel>
+                <Input value="hoge@aaa.com" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>電話番号</FormLabel>
+                <Input value="00-000-0000" isReadOnly />
+              </FormControl>
+            </Stack>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
