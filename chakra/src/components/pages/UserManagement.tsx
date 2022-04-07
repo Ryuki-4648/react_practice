5d63b89a7716bf7a5fbaffa65962f8d5cfdf97ef
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-08 06:55:28 +0900

    画面表示（マウント）時にユーザー一覧を取得

--- chakra/src/components/pages/UserManagement.tsx
+++ chakra/src/components/pages/UserManagement.tsx
@@ -1,18 +1,36 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  /* 画面表示（マウント）時にユーザー一覧を取得したい
+  空配列：初期マウント時一回だけ実行
+  */
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="maiyan"
-          fullName="Mai Shiraishi"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          <WrapItem>
+            <UserCard
+              imageUrl="https://source.unsplash.com/random"
+              userName="maiyan"
+              fullName="Mai Shiraishi"
+            />
+          </WrapItem>
+        </Wrap>
+      )}
+    </>
   );
 });
